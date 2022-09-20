66b8de398f263472bd82dac73ece2301d6cda8ed
farazrb1 <faraz.khan6903@gmail.com>
2022-09-20 10:16:54 -0600

    code update

---
+++ Code/Project1.R
@@ -0,0 +1,7 @@
+### script to make figures for manuscript ###
+### LDP Class Productivity and Reproducibility ###
+### Sept 2022 ###
+### Faraz Khan ###
+### Chaves-Ulloa, Ramsa et al. (2016) Data from: Dissolved organic carbon modulates mercury concentrations 
+### in insect subsidies from streams to terrestrial consumers, 
+### Dryad, Dataset, https://doi.org/10.5061/dryad.mm2t9
